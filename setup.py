83cb68f5a220b0337194495c2e80251af26fc8c2
Jake Howard <git@theorangeone.net>
2020-05-02 18:55:58 +0100

    Bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="django-sri",
-    version="0.0.0",
+    version="0.1.0",
     url="https://github.com/RealOrangeOne/django-sri",
     author="Jake Howard",
     description="Subresource Integrity for Django",

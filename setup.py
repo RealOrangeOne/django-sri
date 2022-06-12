11dcdad67e1a11cc18a99ac4c117fb21564e988e
Jake Howard <git@theorangeone.net>
2022-06-12 21:24:23 +0100

    Drop support for Python 3.7

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     packages=find_packages(include=["sri*"]),
     package_data={"sri": ["py.typed"]},
     install_requires=["Django>=3.2"],
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     keywords="django subresource integrity sri",
     classifiers=[
         "Environment :: Web Environment",
@@ -23,7 +23,6 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",

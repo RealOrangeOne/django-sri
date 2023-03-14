d3ccd26301a4fffe3c1585729820173082362567
Jake Howard <git@theorangeone.net>
2023-03-14 22:33:17 +0000

    Add CI for Python 3.11

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python",
         "Topic :: Internet :: WWW/HTTP",

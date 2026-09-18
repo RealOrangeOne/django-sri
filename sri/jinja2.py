bcf62ff0d69ecaabb18309f0b0276946a5c1fc35
Jake Howard <git@theorangeone.net>
2026-09-18 20:13:46 +0100

    Remove older APIs in favour of simpler ones

--- sri/jinja2.py
+++ sri/jinja2.py
@@ -1,14 +1,14 @@
 from jinja2 import Environment
 from jinja2.ext import Extension
 
-from sri.templatetags.sri import sri_integrity, sri_static
+from sri.templatetags.sri import sri_attrs, sri_integrity
 
 
 class SRIExtension(Extension):
     def __init__(self, environment: Environment) -> None:
         super().__init__(environment)
 
-        environment.globals["sri_static"] = sri_static
+        environment.globals["sri_attrs"] = sri_attrs
         environment.globals["sri_integrity"] = sri_integrity
 
 
--- tests/tests.py
+++ tests/tests.py
@@ -7,8 +7,7 @@ from django.conf import settings
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.core.cache import caches
 from django.core.management import call_command
-from django.template import Context, Engine
-from django.template.loader import render_to_string
+from django.template import engines
 
 import sri
 from sri.algorithm import Algorithm
@@ -23,62 +22,7 @@ def setup_function(*_: Any) -> None:
     shutil.rmtree(settings.STATIC_ROOT, ignore_errors=True)
 
 
-def test_simple_template() -> None:
-    rendered = render_to_string("simple.html")
-    assert (
-        '<script crossorigin="anonymous" integrity="sha256-VROI/fAMCWgkTthVtzzvHtPkkxvpysdZbcqLdVMtwOI=" src="/static/index.js"></script>'
-        in rendered
-    )
-    assert (
-        '<link crossorigin="anonymous" href="/static/index.css" integrity="sha256-fsqAKvNYgo9VQgSc4rD93SiW/AjKFwLtWlPi6qviBxY=" rel="stylesheet" type="text/css">'
-        in rendered
-    )
-
-
-def test_complex_template() -> None:
-    rendered = render_to_string("complex.html")
-    assert (
-        '<script crossorigin="anonymous" integrity="sha256-VROI/fAMCWgkTthVtzzvHtPkkxvpysdZbcqLdVMtwOI=" src="/static/index.js" defer async></script>'
-        in rendered
-    )
-    assert (
-        '<link as="font" crossorigin="anonymous" href="/static/index.woff2" integrity="sha256-hWU2c2zzSsvKYN7tGMnt3t3Oj7GwQZB2aLRhCWYbFSE=" preload>'
-        in rendered
-    ), rendered
-
-
-def test_algorithms_template() -> None:
-    rendered = render_to_string("algorithms.html")
-    assert (
-        '<script crossorigin="anonymous" integrity="sha384-dExnf54EbXTQ1VmweBEJRWX3MPT4xeDV5p71GIX2hpvV+8B/kzo3SObynuveYt9w" src="/static/index.js"></script>'
-        in rendered
-    )
-    assert (
-        '<link crossorigin="anonymous" href="/static/index.css" integrity="sha512-7v9G7AKwpjnlEYhw9GdXu/9G8bq0PqM427/QmgH2TufqEUcjsANEoyCoOkpV8TBCnbQigwNKpMaZNskJG8Ejdw==" rel="stylesheet" type="text/css">'
-        in rendered
-    )
-
-
-def test_jinja2_template() -> None:
-    rendered = render_to_string("complex.j2")
-    assert (
-        '<script crossorigin="anonymous" integrity="sha256-VROI/fAMCWgkTthVtzzvHtPkkxvpysdZbcqLdVMtwOI=" src="/static/index.js" defer async></script>'
-        in rendered
-    )
-    assert (
-        '<link as="font" crossorigin="anonymous" href="/static/index.woff2" integrity="sha256-hWU2c2zzSsvKYN7tGMnt3t3Oj7GwQZB2aLRhCWYbFSE=" preload>'
-        in rendered
-    )
-
-
-@pytest.mark.parametrize("algorithm", sri.Algorithm)
-@pytest.mark.parametrize("file", TEST_FILES)
-def test_generic_algorithm(algorithm: Algorithm, file: str) -> None:
-    val = templatetags.sri_integrity(file, algorithm)
-    assert val.startswith(f"{algorithm.value}-"), val
-
-
-def test_default_algorithm() -> None:
+def test_uses_default_algorithm() -> None:
     val = templatetags.sri_integrity("index.js")
     assert val.startswith(f"{Algorithm.get_default().value}-"), val
 
@@ -101,51 +45,39 @@ def test_default_algorithm_exists() -> None:
 @pytest.mark.parametrize("algorithm", sri.Algorithm)
 @pytest.mark.parametrize("file", TEST_FILES)
 def test_hashes_are_consistent(algorithm: Algorithm, file: str) -> None:
-    digest = sri.hashers.calculate_hash(sri.utils.get_static_path(file), algorithm)
+    digest = sri.calculate_integrity_of_static(file, algorithm)
     caches["default"].clear()
-    digest_2 = sri.hashers.calculate_hash(sri.utils.get_static_path(file), algorithm)
+    digest_2 = sri.calculate_integrity_of_static(file, algorithm)
     assert digest == digest_2
 
 
 @pytest.mark.parametrize("algorithm", sri.Algorithm)
 @pytest.mark.parametrize("file", TEST_FILES)
 def test_integrity(algorithm: Algorithm, file: str) -> None:
-    integrity = sri.integrity.calculate_integrity(
-        sri.utils.get_static_path(file), algorithm
-    )
+    integrity = sri.calculate_integrity_of_static(file, algorithm)
     assert integrity.startswith(algorithm.value)
 
 
 @pytest.mark.parametrize("file", TEST_FILES)
-def test_disable_sri(file: str) -> None:
-    original_value = templatetags.USE_SRI
-    try:
-        templatetags.USE_SRI = False
-        assert "integrity" not in templatetags.sri_static(file)
-    finally:
-        templatetags.USE_SRI = original_value
-
-
-@pytest.mark.parametrize("algorithm", sri.Algorithm)
-@pytest.mark.parametrize("file", TEST_FILES)
-def test_sri_integrity_tag(algorithm: Algorithm, file: str) -> None:
-    assert templatetags.sri_integrity(file, algorithm).startswith(f"{algorithm.value}-")
+def test_disable_sri(settings: Any, file: str) -> None:
+    settings.USE_SRI = False
+    assert templatetags.sri_attrs(file) == ""
 
 
 @pytest.mark.parametrize("file", TEST_FILES)
 def test_unknown_algorithm(file: str) -> None:
     with pytest.raises(ValueError) as e:
-        templatetags.sri_static(file, algorithm="md5")
+        templatetags.sri_integrity(file, algorithm="md5")
     assert e.value.args[0] == "'md5' is not a valid Algorithm"
 
 
 def test_missing_file() -> None:
     with pytest.raises(FileNotFoundError):
-        templatetags.sri_static("foo.js")
+        templatetags.sri_integrity("foo.js")
 
 
 def test_app_file() -> None:
-    templatetags.sri_static("admin/js/core.js")
+    assert templatetags.sri_integrity("admin/js/core.js").startswith("sha256-")
 
 
 @pytest.mark.parametrize("algorithm", sri.Algorithm)
@@ -195,46 +127,104 @@ def test_default_storage(file: str) -> None:
 
 
 @pytest.mark.parametrize(
-    "empty,extra,output",
+    "template,result",
     [
-        ([], {}, ""),
-        (["defer"], {}, " defer"),
-        (["defer", "async"], {}, " defer async"),
-        ([], {"type": "text/javascript"}, ' type="text/javascript"'),
-        (["defer"], {"type": "text/javascript"}, ' type="text/javascript" defer'),
+        (
+            "{% sri_attrs 'index.js' %}",
+            ' crossorigin="anonymous" integrity="sha256-VROI/fAMCWgkTthVtzzvHtPkkxvpysdZbcqLdVMtwOI="',
+        ),
+        (
+            "{% sri_attrs 'index.js' 'sha512' %}",
+            ' crossorigin="anonymous" integrity="sha512-cw/Y369hULp54riZ5vM+t1Q/AkacZnq+JyqpmjXQox0gJKosqpa6CD3mqC2fQHokN13H0fqBQgnfb91lSFAOGQ=="',
+        ),
+        (
+            "{% sri_attrs 'index.css' %}",
+            ' crossorigin="anonymous" integrity="sha256-fsqAKvNYgo9VQgSc4rD93SiW/AjKFwLtWlPi6qviBxY="',
+        ),
+        (
+            "{% sri_attrs 'index.woff2' %}",
+            ' crossorigin="anonymous" integrity="sha256-hWU2c2zzSsvKYN7tGMnt3t3Oj7GwQZB2aLRhCWYbFSE="',
+        ),
     ],
 )
-def test_format_attrs(empty: list, extra: dict, output: str) -> None:
-    elem = templatetags.format_attrs(*empty, **extra)
-    assert elem == output, elem
+def test_attrs(template: str, result: str) -> None:
+    assert (
+        engines["django"].from_string("{% load sri %}" + template).render({}) == result
+    )
 
 
 @pytest.mark.parametrize(
     "template,result",
     [
         (
-            "{% sri_attrs 'index.js' %}",
-            'crossorigin="anonymous" integrity="sha256-VROI/fAMCWgkTthVtzzvHtPkkxvpysdZbcqLdVMtwOI="',
+            "{{ sri_attrs('index.js') }}",
+            ' crossorigin="anonymous" integrity="sha256-VROI/fAMCWgkTthVtzzvHtPkkxvpysdZbcqLdVMtwOI="',
         ),
         (
-            "{% sri_attrs 'index.js' 'sha512' %}",
-            'crossorigin="anonymous" integrity="sha512-cw/Y369hULp54riZ5vM+t1Q/AkacZnq+JyqpmjXQox0gJKosqpa6CD3mqC2fQHokN13H0fqBQgnfb91lSFAOGQ=="',
+            "{{ sri_attrs('index.js', 'sha512') }}",
+            ' crossorigin="anonymous" integrity="sha512-cw/Y369hULp54riZ5vM+t1Q/AkacZnq+JyqpmjXQox0gJKosqpa6CD3mqC2fQHokN13H0fqBQgnfb91lSFAOGQ=="',
         ),
         (
-            "{% sri_attrs 'index.css' %}",
-            'crossorigin="anonymous" integrity="sha256-fsqAKvNYgo9VQgSc4rD93SiW/AjKFwLtWlPi6qviBxY="',
+            "{{ sri_attrs('index.css') }}",
+            ' crossorigin="anonymous" integrity="sha256-fsqAKvNYgo9VQgSc4rD93SiW/AjKFwLtWlPi6qviBxY="',
         ),
         (
-            "{% sri_attrs 'index.woff2' %}",
-            'crossorigin="anonymous" integrity="sha256-hWU2c2zzSsvKYN7tGMnt3t3Oj7GwQZB2aLRhCWYbFSE="',
+            "{{ sri_attrs('index.woff2') }}",
+            ' crossorigin="anonymous" integrity="sha256-hWU2c2zzSsvKYN7tGMnt3t3Oj7GwQZB2aLRhCWYbFSE="',
         ),
     ],
 )
-def test_attrs(template: str, result: str) -> None:
+def test_attrs_jinja2(template: str, result: str) -> None:
+    assert engines["jinja2"].from_string(template).render({}) == result
+
+
+@pytest.mark.parametrize(
+    "template,result",
+    [
+        (
+            "{% sri_integrity 'index.js' %}",
+            "sha256-VROI/fAMCWgkTthVtzzvHtPkkxvpysdZbcqLdVMtwOI=",
+        ),
+        (
+            "{% sri_integrity 'index.js' 'sha512' %}",
+            "sha512-cw/Y369hULp54riZ5vM+t1Q/AkacZnq+JyqpmjXQox0gJKosqpa6CD3mqC2fQHokN13H0fqBQgnfb91lSFAOGQ==",
+        ),
+        (
+            "{% sri_integrity 'index.css' %}",
+            "sha256-fsqAKvNYgo9VQgSc4rD93SiW/AjKFwLtWlPi6qviBxY=",
+        ),
+        (
+            "{% sri_integrity 'index.woff2' %}",
+            "sha256-hWU2c2zzSsvKYN7tGMnt3t3Oj7GwQZB2aLRhCWYbFSE=",
+        ),
+    ],
+)
+def test_integrity_tag(template: str, result: str) -> None:
     assert (
-        Engine.get_default()
-        .from_string("{% load sri %}" + template)
-        .render(Context())
-        .strip()
-        == result
+        engines["django"].from_string("{% load sri %}" + template).render({}) == result
     )
+
+
+@pytest.mark.parametrize(
+    "template,result",
+    [
+        (
+            "{{ sri_integrity('index.js') }}",
+            "sha256-VROI/fAMCWgkTthVtzzvHtPkkxvpysdZbcqLdVMtwOI=",
+        ),
+        (
+            "{{ sri_integrity('index.js', 'sha512') }}",
+            "sha512-cw/Y369hULp54riZ5vM+t1Q/AkacZnq+JyqpmjXQox0gJKosqpa6CD3mqC2fQHokN13H0fqBQgnfb91lSFAOGQ==",
+        ),
+        (
+            "{{ sri_integrity('index.css') }}",
+            "sha256-fsqAKvNYgo9VQgSc4rD93SiW/AjKFwLtWlPi6qviBxY=",
+        ),
+        (
+            "{{ sri_integrity('index.woff2') }}",
+            "sha256-hWU2c2zzSsvKYN7tGMnt3t3Oj7GwQZB2aLRhCWYbFSE=",
+        ),
+    ],
+)
+def test_integrity_tag_jinja2(template: str, result: str) -> None:
+    assert engines["jinja2"].from_string(template).render({}) == result
